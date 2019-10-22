db975fee6220f1e01695c1e3ccfb0634f1ee892b
jefshi <jefshi@yeah.net>
2019-10-22 16:58:27 +0800

    应用层调用 bspatch 合并增量包

--- app/src/main/java/com/demo/hdiffpatch/MainActivity.java
+++ app/src/main/java/com/demo/hdiffpatch/MainActivity.java
@@ -1,29 +1,39 @@
 package com.demo.hdiffpatch;
 
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+        findViewById(R.id.btn_permission).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        2);
+            }
+        });
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+        findViewById(R.id.btn_bspatch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final File old = new File(Environment.getExternalStorageDirectory(), "old.apk");
+                final File out = new File(Environment.getExternalStorageDirectory(), "out.apk");
+                final File patch = new File(Environment.getExternalStorageDirectory(), "PATCH.patch");
+                HDiffPatchUtil.hpatchz(old, out, patch);
+            }
+        });
+
+    }
 }
